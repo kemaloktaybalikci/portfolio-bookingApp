c5c78a467f81d9401977f0ac3b5887ada16a5d0e
kemaloktaybalikci <kemaloktaybalikci@gmail.com>
2022-07-20 08:52:02 +0300

    auth, hotelmodel, hotelcontroller, usermodel, usercontroller has been done

---
+++ routes/rooms.js
@@ -0,0 +1,15 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+
+const router = express.Router();
+//CREATE
+
+//UPDATE
+
+//DELETE
+
+//GET
+
+//GET ALL
+
+export default router;
